a0e6ac842aad79593c08a05c613e1360a565144d
Karlo Melendez-Pena <mgkarlo@gmail.com>
2020-07-10 22:44:20 -0400

    Particle2D Node + AudioStreamPlayer Node

--- Bullet.gd
+++ Bullet.gd
@@ -1,9 +1,15 @@
 extends RigidBody2D
 
+const HitEffect = preload("res://HitEffect.tscn")
 
 func _ready():
 	pass
 
-
 func _on_Visibility_screen_exited():
 	queue_free()
+
+func create_hit_effect():
+	var main = get_tree().current_scene
+	var hitEffect = HitEffect.instance()
+	main.add_child(hitEffect)
+	hitEffect.global_position = global_position
--- Enemy.gd
+++ Enemy.gd
@@ -13,17 +13,22 @@ func _process(delta):
 
 func _on_Enemy_body_entered(body):
 	body.queue_free()
+	body.create_hit_effect()
 	ARMOR -= 1
 	if ARMOR <= 0:
-		var main = get_tree().current_scene
-		if main.is_in_group("World"):
-			main.score += 10
+		add_to_score()
+		create_explosion()
 		queue_free()
+
+func add_to_score():
+	var main = get_tree().current_scene
+	if main.is_in_group("World"):
+		main.score += 10
 
 func _on_Visibility_screen_exited():
 	queue_free()
 
-func _exit_tree():
+func create_explosion():
 	var main = get_tree().current_scene
 	var explosionEffect = ExplosionEffect.instance()
 	main.add_child(explosionEffect)
