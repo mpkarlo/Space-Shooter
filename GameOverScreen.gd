cd0da6ab83189841e9221a2ffc9a95aef6813eaa
Karlo Melendez-Pena <mgkarlo@gmail.com>
2020-07-10 23:51:59 -0400

    Dictionaries + Saving and Loading with JSON

--- GameOverScreen.gd
+++ GameOverScreen.gd
@@ -1,9 +1,14 @@
 extends Node
 
+onready var highScoreLabel = $HighScoreLabel
 
 func _ready():
-	pass
+	set_highscore_label()
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene("res://StartMenu.tscn")
+
+func set_highscore_label():
+	var save_data = SaveAndLoad.load_data_from_file()
+	highScoreLabel.text = "High Score: " + str(save_data.highscore)
--- World.gd
+++ World.gd
@@ -11,7 +11,13 @@ func set_score(value):
 	score = value
 	scoreLabel.text = "Score = " + str(score)
 
+func update_save_data():
+	var save_data = SaveAndLoad.load_data_from_file()
+	if score > save_data.highscore:
+		save_data.highscore = score
+		SaveAndLoad.save_data_to_file(save_data)
 
 func _on_Ship_player_death():
+	update_save_data()
 	yield(get_tree().create_timer(1), "timeout")
 	get_tree().change_scene("res://GameOverScreen.tscn")
